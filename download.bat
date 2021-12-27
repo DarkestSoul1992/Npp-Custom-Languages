19f1de42e6a8a44869b46a9eb8fb5e0c29736953
TheAlmostGenius <johnwolfe001@hotmail.co.uk>
2021-12-27 11:54:21 +0000

    Added NES file...

--- download.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-git pull https://github.com/TheAlmostGenius/Npp-Custom-Languages.git
--- upload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git add .\*.*
-git commit -a
-git push --set-upstream https://github.com/TheAlmostGenius/Npp-Custom-Languages.git main -f
